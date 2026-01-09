2ffb6382321e3f1e083ca239d121b534bf5b5ef1
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-01-08 23:33:55 -0500

    Time: 66 ms (82.27%), Space: 28.1 MB (5.18%) - LeetHub

--- 0931-maximum-frequency-stack/0931-maximum-frequency-stack.py
+++ 0931-maximum-frequency-stack/0931-maximum-frequency-stack.py
@@ -1,38 +1,21 @@
 class FreqStack:
 
     def __init__(self):
-        self.counts = {}  # map element to num occurences
-        self.stack1 = []
-        self.stack2 = []  # use to hold elements after the most freq is popped
+        self.freq = defaultdict(list)  # freq -> [list of ele]
+        self.counts = defaultdict(int)  # ele -> freq
+        self.maxCount = 0
 
     def push(self, val: int) -> None:
-        self.stack1.append(val)
-        self.counts[val] = self.counts.get(val, 0) + 1
+        self.counts[val]+=1
+        self.freq[self.counts[val]].append(val)
+        self.maxCount = max(self.maxCount, self.counts[val])
 
     def pop(self) -> int:
-        maxCount = -1
-        for ele, count in self.counts.items():
-            maxCount = max(maxCount, count)
-        ind = 0
-        for ind in range(len(self.stack1)-1, -1, -1):
-            ele = self.stack1[ind]
-            if self.counts[ele] == maxCount:
-                break
-        
-        while self.stack1:
-            poppedEle = self.stack1.pop()
-            if poppedEle == ele:
-                break
-            self.stack2.append(poppedEle)
-        
-        while self.stack2:
-            self.stack1.append(self.stack2.pop())
-        
-        self.counts[ele] = max(self.counts[ele]-1, 0)
-        return ele
-
-        
-        
+        popped_ele = self.freq[self.maxCount].pop()
+        self.counts[popped_ele] = max(self.counts[popped_ele]-1, 0)
+        if len(self.freq[self.maxCount]) == 0:
+            self.maxCount-=1
+        return popped_ele
 
 
 # Your FreqStack object will be instantiated and called as such:

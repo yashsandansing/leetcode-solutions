e8d4fcad7ab3d578d20245d0c0b73391c98e8009
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-01-10 12:35:42 -0500

    Time: 179 ms (27.37%), Space: 20.7 MB (5.51%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        highest, lowest = max(piles), 1
+        bestEatingRate = highest
+
+        while highest>=lowest:
+            currEatingRate = (highest + lowest) // 2
+            
+            timeToEat = 0
+            for pile in piles:
+                timeToEat += (pile//currEatingRate) + (pile%currEatingRate > 0)
+            
+            if timeToEat > h:
+                lowest = currEatingRate + 1
+            else:
+                highest = currEatingRate - 1
+                bestEatingRate = min(bestEatingRate, currEatingRate)
+
+        return bestEatingRate

8acc558cbfefb710d4571786073a77cd73c6007b
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-01-29 15:34:53 -0500

    Time: 35 ms (40.79%), Space: 36.7 MB (30.99%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,8 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # solution: store a hashmap of last counts
         lastCount = dict() # element: last_index
 
+        # for each element, check if element has occured previously
+        # if it has, subtract the indices to get the result
+        # if result <= k return true, else false
         for ind, num in enumerate(nums):
+            
             if lastCount.get(num) is not None and abs(lastCount[num] - ind) <= k:
                 return True
             lastCount[num] = ind

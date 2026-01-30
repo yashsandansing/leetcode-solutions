bb49ebd28581e13a14605f34201a68519aeae0d0
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-01-29 20:36:35 -0500

    Time: 0 ms (100%), Space: 20.7 MB (26.8%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -14,22 +14,25 @@ class Solution:
         # how to solve it?
         # set l to 0 and r to len(arr) - k
         # bcs need to return arr[l:l+k]
-        # use two pointers -> mid and (mid+k)
-        # at each step check if x<=arr[mid] or x>arr[mid + k]
-        # if x<=arr[mid] -> set r to mid (no concern with right half to find l)
-        # if x>arr[mid + k] -> set l to mid + 1, because the prev ones are irrelevant
-        # if arr[mid] < x < arr[mid + k] -> check which element x is closer to and update
-        # l or r accordingly
+        
+        
         l = 0
         r = len(arr) - k
 
         while l < r:
             m = (l + r) // 2
+            # use two pointers -> mid and (mid+k)
+            # at each step check if x<=arr[mid] or x>arr[mid + k]
+            # if x<=arr[mid] -> set r to mid (no concern with right half to find l)
             if x <= arr[m]:
                 r = m
+            # if x>arr[mid + k] -> set l to mid + 1, because the prev ones are irrelevant
+            # and we dont need that starting point
             elif x > arr[m + k]:
                 l = m + 1
             else:
+                # if arr[mid] < x < arr[mid + k] -> check which element x is closer to and update
+                # l or r accordingly
                 diff_low = abs(arr[m] - x)
                 diff_high = abs(arr[m + k] - x)
 

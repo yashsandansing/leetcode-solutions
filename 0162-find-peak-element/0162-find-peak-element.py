476a51039b7293ba1e25675f61fbddb0a8d02b9c
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-14 22:22:14 -0500

    Time: 0 ms (100%), Space: 19.4 MB (56.58%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,13 +1,16 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # base peak -> first element
-        # [0, 1, ..., n-2, n-1]
-        # log n -> binary search
-        # [0, 1, 2, 1, 5, 7, 6]
-        peak = nums[0]
-        n = len(nums)
-        for ind in range(n - 1):
-            if nums[ind] > nums[ind + 1]:
-                return ind
-            peak = nums[ind]
-        return n - 1
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+
+            # if upward slope, go up
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            # it's a downward slope ahead, go right
+            else:
+                r = mid
+        
+        return l

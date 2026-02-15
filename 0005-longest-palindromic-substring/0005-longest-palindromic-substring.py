9ea376fdf9df736611c9132ee62afac80ca70620
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-15 16:56:49 -0500

    Time: 275 ms (60.43%), Space: 19.3 MB (56.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,24 +1,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        start = end = 0
+        # edge case -> empty or 1 len string
+        if len(s) <= 1:
+            return s
 
-        for c in range(len(s)):
-            # for odd checks
-            i = j = c
-            while i>0 and j<len(s)-1 and s[i-1] == s[j+1]:
-                # print(i, j)
-                i -= 1
-                j += 1
-            if j - i > end - start:
-                start, end = i, j
-            
-            i, j = c, c+1
-            if j<len(s) and s[i] == s[j]:
-                while i>0 and j<len(s)-1 and s[i-1] == s[j+1]:
-                    i -= 1
-                    j += 1
-                
-                if j - i > end - start:
-                    start, end = i, j
+        def expand(l, r):
+            # loop to check if current elements are same
+            # if they are expand and check next outer elements
+            while l>=0 and r<=len(s)-1 and s[l] == s[r]:
+                l -= 1
+                r += 1
+            # return l+1 since thats our starting point
+            # return r to stop
+            # why not return r-1 too -> could be less than l+1 (in even cases like 'cb')
+            # also we anyways add +1 when calculating and updating string
+            return l+1, r
+
+        res = ""
+
+        for ind in range(len(s) - 1):
+
+            # for odd
+            l, r = expand(ind, ind)
+            if r-l > len(res):
+                res = s[l:r]
+
+            # even-len palindromes
+
+            # ind + 1 does not throw an IndexError
+            # due to the checks implemented in the expand function
+            l, r = expand(ind, ind + 1)
+            if r-l > len(res):
+                res = s[l:r]
         
-        return s[start: end+1]
+        return res

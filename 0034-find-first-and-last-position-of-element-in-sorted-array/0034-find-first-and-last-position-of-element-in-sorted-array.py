f04a5eb139b3e4d3343c7cdc96854e79764c5a57
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-17 19:41:40 -0500

    Time: 0 ms (100%), Space: 20.7 MB (21.82%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,41 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # optimal solution
-
-        def search(target):
+        
+        def binary_search(search_left: bool) -> int:
             l = 0
-            r = len(nums)
+            r = len(nums) - 1
+            # index to return if found element
+            # if not found, return -1
+            ind = -1
 
-            while l < r:
+            while l<=r:
                 m = (l + r) // 2
 
-                if target <= nums[m]:
-                    r = m
-                else:
+                # normal binary search if elements
+                # are greater or lesser than target
+                if target > nums[m]:
                     l = m + 1
-            return l
-        
-        first = search(target)
-        last = search(target + 1) - 1
+                elif target < nums[m]:
+                    r = m - 1
+                
+                # if target hit
+                else:
+                    # store index in ind
+                    ind = m
+                    # search_left => we want to find the leftmost
+                    # i.e. starting element -> go left by decrementing
+                    # the right boundary
+                    if search_left:
+                        r = m - 1
+                    
+                    # if not search_left, we need to search_right
+                    # increment left to go right in that case
+                    else:
+                        l = m + 1
 
-        if first <= last:
-            return [first, last]
+            return ind
         
-        return [-1, -1]
+        left = binary_search(True)
+        right = binary_search(False)
+
+        return [left, right]

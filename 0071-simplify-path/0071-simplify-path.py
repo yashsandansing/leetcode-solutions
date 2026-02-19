9ed438452a18332ec222f04e743144bda9f342d9
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-18 20:58:23 -0500

    Time: 3 ms (40.9%), Space: 19.4 MB (20.94%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,18 +1,26 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        # no trailing slashes
+        # if .. => remove the prev element (if exists)
+        # if . => dont add anything
+        # return at least / to /path
+        
+        # empty list -> return /
+        # stack -> .. pop from the stack
+        # . -> dont add anything
+        # /.join(stack)
+
+        path = path.split("/")
+        print(path)
         stack = []
-        curr = ""
 
-        for c in path+'/':
-            if c == "/":
-                if curr == "..":
-                    # print('here', stack, curr)
-                    if stack:
-                        stack.pop()
-                elif curr!="" and curr!=".":
-                    stack.append(curr)
-                curr = ""
+        for loc in path:
+            if loc == "." or not loc:
+                continue
+            elif loc == "..":
+                if stack:
+                    stack.pop()
             else:
-                curr+=c
-
-        return "/" + '/'.join(stack)
+                stack.append(loc)
+        
+        return "/" + "/".join(stack)

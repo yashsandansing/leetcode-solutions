1e346373d3913902fbfa3829211462665f496400
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-22 15:36:28 -0500

    Time: 7 ms (71.53%), Space: 23.2 MB (32.95%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,32 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        # len -> 1 to 10^4
+        # can start == end? yes
+        
+        # sorted? -> no
+        # (1, 4), (4, 7) => (1, 7)
+
+        # thoughts
+        # sort it by their start times (secondary is end)
+        # for a pair check if they are intersecting
+        # end[0] >= start[1] => yes
+        # merge them => [min(start), max(end)]
+        # stack = [] => [(1, 5)]
+
+        # guessing that it sorts by i[0] and i[1]
+        intervals.sort()
+
+        stack = [intervals[0]]
+
+        for idx in range(len(intervals)):
+            curr_start, curr_end = intervals[idx]
+            prev_start, prev_end = stack[-1]
+
+            if prev_end >= curr_start:
+                stack.pop()
+                stack.append([prev_start, max(prev_end, curr_end)])
+            else:
+                stack.append([curr_start, curr_end])
+
+        return stack

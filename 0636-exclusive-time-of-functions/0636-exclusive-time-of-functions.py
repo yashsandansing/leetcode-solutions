10e81474c0edcce88d22c71b491bb20c4ccf31fd
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-21 23:39:41 -0500

    Time: 6 ms (68.78%), Space: 19.4 MB (45.46%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,34 +1,24 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        # at least one? yes
-        # no same start or end for 2 diff events
-        # each func has an end
-        # stack = []
-        # 0, 0
-        # te = 4
-        # [3, 4]
-        # 0, 0, 5
-        # 7 - 0 + 1 - 4
-        # [9]
-        
-        time_elapsed = 0
-        # stack -> element_id, start_time, elapsed_time
         stack = []
-        res = [0]*n
-        total_elapsed = 0
+        res = [0] * n
+        prev = 0
 
         for log in logs:
-            ele, type_, time = log.split(":")
-            ele = int(ele)
-            time = int(time)
-            if type_ == 'start':
-                stack.append([ele, time, 0])
-            else:
-                popped_ele, start_time, elapsed = stack.pop()
-                time_to_complete = time - start_time + 1 - elapsed
-                res[popped_ele] += time_to_complete
+            process, type, time = log.split(":")
+            process, time = int(process), int(time)
+
+            if type == "start":
                 if stack:
-                    stack[-1][-1] += time_to_complete + elapsed
-            print(stack)
+                    prev_process = stack[-1]
+                    res[prev_process] += time - prev
+
+                stack.append(process)
+                prev = time
+
+            else:
+                curr = stack.pop()
+                res[curr] += time - prev + 1
+                prev = time + 1
         
         return res

55fa83446bf4a8be676e058c8829f34014e22c6b
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-02-21 22:59:56 -0500

    Time: 6 ms (95.18%), Space: 20.6 MB (51.87%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,5 +1,9 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+
+        # store at least one character in the string
+        # so that if s starts with a valid character
+        # start calculating from -1
         stack = [-1]
         max_valid = 0
 
@@ -8,6 +12,11 @@ class Solution:
                 stack.append(idx)
             else:
                 stack.pop()
+                # if stack == 0, that means current character is the latest
+                # invalid character (closing before opening or not matching opening)
+                # append current idx to stack since it would be the only character in
+                # the stack. if an opening is added, it would be popped by another closing
+                # if another closing is added, this one would be popped and replaced instead
                 if len(stack) == 0:
                     stack.append(idx)
                 else:

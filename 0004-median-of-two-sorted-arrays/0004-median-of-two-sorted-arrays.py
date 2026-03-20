8b00f31e74cf3e404fbf7a2ceb528805ccda6d2c
Yash Sandansing <48054260+yashsandansing@users.noreply.github.com>
2026-03-20 10:53:14 -0400

    Time: 0 ms (100%), Space: 19.5 MB (68.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,45 +1,32 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         total = len(nums1) + len(nums2)
-        half = total // 2 # works for both even-odd arrays
-        # in case of odd arrays, 2nd half has +1 element
-        A, B = nums1, nums2
-        # want A to be smaller
-        if len(B) < len(A):
-            A, B = B, A
+        half = total // 2
 
+        if len(nums2) < len(nums1):
+            nums1, nums2 = nums2, nums1
+        
         l = 0
-        r = len(A) - 1
+        r = len(nums1) - 1
 
-        # do a binary search on A and B to find median
         while True:
-            midA = (l + r) // 2
-            
-            # midA + 1 -> convert from index to count
-            # subtract from half -> taking only required elements from 2nd array
-            # final -1 -> converting from count to index
-            midB = half - (midA + 1) - 1
+            m = (l + r) // 2
+            n = half - m - 2
 
-            # take values around boundaries
-            # -inf and inf is taken for ease and to say that this half
-            # doesnt have enough/any elements. select inf (or *-1)
-            left_A  = A[midA] if midA >= 0 else float('-inf')  # take right-most minimum value in A
-            right_A = A[midA+1] if midA + 1 < len(A) else float('inf')  # take left-most maximum value in A
-            left_B  = B[midB] if midB >= 0 else float('-inf')
-            right_B = B[midB+1] if midB + 1 < len(B) else float('inf')
+            A1 = nums1[m] if m >= 0 else float('-inf')
+            B1 = nums2[n] if n >= 0 else float('-inf')
 
-            # check if median condition is satisfied
-            # since individual arrays are sorted, we check for A and B
-            if left_A <= right_B and left_B <= right_A:    
-                # if even we will take sum of two medians
+            A2 = nums1[m + 1] if m + 1 < len(nums1) else float('inf')
+            B2 = nums2[n + 1] if n + 1 < len(nums2) else float('inf')
+
+            if A1 <= B2 and B1 <= A2:
                 if total % 2 == 0:
-                    return (max(left_A, left_B) + min(right_A, right_B))/2
-                # if odd we want just one element
-                return min(right_B, right_A)
-            # if took too much from A, reduce how much you took
-            elif left_A > right_B:
-                r -= 1
+                    return (max(A1, B1) + min(A2, B2)) / 2
+                
+                return min(A2, B2)
             
-            # didnt take enough elements from A
             else:
-                l += 1
+                if A1 > B2:
+                    r = m - 1
+                else:
+                    l = m + 1
